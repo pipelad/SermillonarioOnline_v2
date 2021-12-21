47aaa6319ae83b48ec18ef00b181e29bc7ff29fd
pipelad <86892383+pipelad@users.noreply.github.com>
2021-12-21 09:02:04 -0500

    added sass

--- js/juego.js
+++ js/juego.js
@@ -3,24 +3,27 @@
 
 //// 1. Sección para Variables globales
 
-const xmlhttp = new XMLHttpRequest();
+const xmlhttp = new XMLHttpRequest(),
+	  loadCircle = document.querySelector('#loader--circle'),
+	  loadPath = document.querySelector('#loader--chulo'),
+	  areaRespuesta = document.querySelector('#respuestas'),
+	  startBtn = document.querySelector('#start-btn'),
+	  puntajeActual = document.querySelector('#puntos'), 
+	  ayuda = document.querySelector('#ayuda5050'),
+	  pregarea = document.querySelector('#pregunta'), 
+	  bien = document.querySelector('#correcto'), 
+	  mal = document.querySelector('#incorrecto'), 
+	  final  = document.querySelector('#final'), 
+	  finTime = document.querySelector('#tiempofinal'), 
+	  finPunt = document.querySelector('#puntajefinal'), 
+	  aprender = document.querySelector('#aprender');
 
 let	cuestionarioObj = '',
 	cuestionario = [],
 	number = '', 
 	puntaje = 0,
-	startBtn = document.querySelector('#start-btn'),
-	puntajeActual = document.querySelector('#puntos'), 
-	ayuda = document.querySelector('#ayuda5050'), 
-	currentAyuda = [0, 1, 2, 3], 
-	pregarea = document.querySelector('#pregunta'), 
-	bien = document.querySelector('#correcto'), 
-	mal = document.querySelector('#incorrecto'), 
-	final  = document.querySelector('#final'), 
-	finTime = document.querySelector('#tiempofinal'), 
-	finPunt = document.querySelector('#puntajefinal'), 
-	ayudaUsed = false,
-	aprender = document.querySelector('#aprender');
+	currentAyuda = [0, 1, 2, 3]; // diseñado el 50/50 a base de 4 preguntas a futuro pensar en solución dinámica
+	
 
 
 //// 2. Sección buscar archivo cuestionario
@@ -29,6 +32,14 @@ xmlhttp.onreadystatechange = function() {
 	if(this.readyState == 4 && this.status == 200) {
         cuestionarioObj = JSON.parse(this.responseText);
         cuestionario = cuestionarioObj.Cuestionario;
+        setTimeout(function() {
+        	animateLoader(loadPath, false, 2, 0.7);
+        	loadPath.classList.remove('hid');
+        	setTimeout(function() {
+	        	document.querySelector('#loader').classList.add('hid');
+	        }, 2000)
+        }, 1000)
+        
         //console.log(cuestionarioObj);
     } else if(this.readyState == 4 && this.status != 200){
     	console.log(`Error: ${this.statusText}`);
@@ -46,7 +57,7 @@ function preguntepues() {
 	if(cuestionario.length !== 0) {
 		number = Math.floor(Math.random() * cuestionario.length);
 		pregarea.innerText = cuestionario[number].pregunta;
-		currentAyuda.splice(cuestionario[number].correcta, 1);
+		currentAyuda.splice(cuestionario[number].correcta, 1); //splice de la respuesta correcta para que sea Array de respuestas incorrectas
 		//console.log(currentAyuda);
 		//console.log(cuestionario[number].correcta);
 		respuestas();
@@ -59,26 +70,36 @@ function preguntepues() {
 	}
 } 
 
-//// 4. Función para imprimir respuestas, 4 fijas, para mejor practica y respuestas variables en un futuro usar appendChild
+//// 4. Función para imprimir respuestas en ordern random, 4 fijas, para mejor practica y respuestas variables en un futuro usar innerHtml
 
 function respuestas() {
+	// version 1, con 4 respuestas fijas, es restrictivo.
+	/*
 	let xRespuesta = Array.from(document.querySelectorAll('.respclic'));
 	for(let i = 0; i < cuestionario[number].respuestas.length; i++) {
 		let n = Math.floor(Math.random() * xRespuesta.length);
-		xRespuesta[n].innerHTML = '<div class="respuesta" onclick="siguiente(' + i + ')"><div class="txt">' + cuestionario[number].respuestas[i] + '</div></div>';	
-		xRespuesta[n].setAttribute('id', "btn_" + i);
+		xRespuesta[n].innerHTML = `<div class="respuesta" onclick="siguiente(${i}" id="btn_${i}"><div class="txt">${cuestionario[number].respuestas[i]}</div></div>`
 		xRespuesta.splice(n, 1);
 	}
+	*/
+	// version innerHtml -- en esta versión no depende de un numero que corresponde a la respuesta correcta, en el momento limitado a 4 por la ayuda 50/50 
+	
+	let xRespuestas = [...cuestionario[number].respuestas].sort(() => Math.random() - 0.5); 
+	// usar sort para randomizar aparición de las respuestas esto implica un problma porque la key del array ya no equivaldría a la respuesta correcta, complica la función actual del juego
+	for(let i = 0; i < xRespuestas.length; i++ ) {
+		let respuestaLiteral = `<div class="rcont"><img src="images/pregunta-02.png"><div class="pc respclic"><div class="respuesta" onclick="siguiente(event)" id="btn_${i}">${cuestionario[number].respuestas[i]}</div></div></div>`;
+		areaRespuesta.innerHTML += respuestaLiteral;
+	}
+	
 }
 
 
-/// siguiente == funcion ejecutada cuando se responde.
+//// 5. Función siguiente ==> ejecutada cuando se responde. evalua el evento contra el objeto para determinar si acerto o se equivocó el usuario
 
-bien.addEventListener('click', function() { bien.classList.add('hid'); });
-mal.addEventListener('click', function() { mal.classList.add('hid'); });
-
-function siguiente(e) {
-	if (e == cuestionario[number].correcta) {
+function siguiente(event) {
+	const e = event.target.innerText;
+	const correcta = String(cuestionario[number].respuestas[cuestionario[number].correcta]);
+	if (e === correcta) {
 		cuestionario.splice(number, 1); /// remover pregunta para no repetir
 		puntaje++;
 		puntajeActual.innerHTML = puntaje;
@@ -93,29 +114,33 @@ function siguiente(e) {
 	}
 	// nueva pregunta 
 	currentAyuda = [0, 1, 2, 3];
-	if(ayudaUsed === true) {
-		for (i = 0; i < currentAyuda.length; i++) {
-			document.getElementById('btn_' + currentAyuda[i]).classList.remove('hid');
-		}
-	}
+	areaRespuesta.innerHTML = '';
 	preguntepues(); 
+	
 }
 
-/// Funcion para ayuda 50/50
+//// 6. Función ocultar intermedio y continuar juego, los intermedios informan sobre el intento anterior
+
+bien.addEventListener('click', function() { bien.classList.add('hid'); });
+mal.addEventListener('click', function() { mal.classList.add('hid'); });
+
+//// 7. Función para ayuda 50/50, elimina 2 respuestas incorrectas del Array de respuestas incorrectas es decir 'currentAyuda' con el splice de la respuesta correcta
 
 ayuda.addEventListener('click', ayuda50);
 
 function ayuda50() {
 	currentAyuda.splice(Math.floor(Math.random() * currentAyuda.length), 1);
 	let ocultarDos = currentAyuda.length;
-	for (i = 0; i < ocultarDos; i++) {
+	// oculta dos elmentos incorrectos
+	for (let i = 0; i < ocultarDos; i++) {
 		document.getElementById('btn_' + currentAyuda[i]).classList.add('hid');
 	}
-	ayuda.classList.add('hid');
-	ayudaUsed = true;
+	// ayuda de 50/50 consumida
+	// ayuda.classList.add('hid'); v1.0 ocultar
+	document.querySelector('#ayuda').innerHTML = ''; //v2.0 eliminar 
 }
 
-/// temporizador
+//// 8. temporizador
 
 function startTimer() {
   	let presentTime = document.getElementById('timer').innerHTML,
@@ -133,7 +158,30 @@ function checkSecond(sec) {
 }
 
 
-//// X. inicializar le juego.
+//// 9. inicializar le juego.
+
+
+//// 9.1 Loader function
+
+function animateLoader(item, repeat, begin, duration) {
+
+	const pathLenght = item.getTotalLength();
+
+	item.setAttribute('stroke-dasharray', pathLenght);
+	item.setAttribute('stroke-dashoffset', pathLenght);
+
+	let repeater = repeat === true ? 'repeatCount="indefinite"' : 'fill="freeze"';
+
+	item.innerHTML = `<animate attributeName="stroke-dashoffset" begin="${Number(begin)}s" dur="${Number(duration)}s" to="0" ${repeater} />`;
+
+	console.log(pathLenght);
+
+}
+
+
+//// 9.2 accion inicial
+animateLoader(loadCircle, true, 0, 1.5);
+
 startBtn.addEventListener('click', function(e) {
 	preguntepues();
 	startTimer();
